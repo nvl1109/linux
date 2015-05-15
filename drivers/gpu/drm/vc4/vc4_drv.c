a5f31ce8d3901bfb70b4d5e1ed44ebf7036503e3
Eric Anholt <eric@anholt.net>
2015-05-15 13:04:46 -0700

    drm/vc4: Make sure we clean up GEM if probe fails.
    We often return -EPROBE_DEFER from the various components, and we
    would have left a bunch of GEM stuff running and allocated.  Cleans up
    a boot-time warning about how we leaked the 256k of overflow_mem.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_drv.c
+++ drivers/gpu/drm/vc4/vc4_drv.c
@@ -204,7 +204,7 @@ static int vc4_drm_bind(struct device *dev)
 
 	ret = component_bind_all(dev, drm);
 	if (ret)
-		goto unref;
+		goto gem_destroy;
 
 	ret = drm_dev_register(drm, 0);
 	if (ret < 0)
@@ -228,6 +228,8 @@ unregister:
 	drm_dev_unregister(drm);
 unbind_all:
 	component_unbind_all(dev, drm);
+gem_destroy:
+	vc4_gem_destroy(drm);
 unref:
 	drm_dev_unref(drm);
 	vc4_bo_cache_destroy(drm);
